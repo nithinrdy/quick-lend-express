d04e92cf3d18a4ad189b68d3c145be8fe7ae7367
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-02-12 02:35:00 +0530

    add community join route + handler

--- src/controllers/communityController.ts
+++ src/controllers/communityController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Community from "../models/Community";
+import User from "../models/User";
 
 export const handleCommunityCreation = async (req: Request, res: Response) => {
 	const { id, name, description, latitude, longitude } = req.body;
@@ -40,12 +41,44 @@ export const handleFetchCommunities = async (req: Request, res: Response) => {
 	try {
 		const communities = await Community.find().exec();
 
-		res
-			.status(200)
-			.json({
-				message: "Communities fetched successfully.",
-				communities: communities,
-			});
+		res.status(200).json({
+			message: "Communities fetched successfully.",
+			communities: communities,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+};
+
+export const handleJoinCommunity = async (req: Request, res: Response) => {
+	const { communityId } = req.body;
+	const { username } = req.body;
+
+	if (!communityId) {
+		return res.status(400).json("Community ID is required.");
+	}
+
+	try {
+		const user = await User.findOne({ username: username }).exec();
+		if (!user) {
+			return res.status(400).json("User could not be found.");
+		}
+
+		const community = await Community.findOne({
+			communityId: communityId,
+		}).exec();
+		if (!community) {
+			return res.status(400).json("Community could not be found.");
+		}
+
+		await user.updateOne({ community: communityId }).exec();
+
+		const updatedUser = await User.findOne({ username: username }).exec();
+
+		res.status(200).json({
+			message: "User joined community successfully.",
+			updatedUser: updatedUser,
+		});
 	} catch (err) {
 		res.status(500).json(err);
 	}
--- src/routes/communities.ts
+++ src/routes/communities.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
 	handleCommunityCreation,
 	handleFetchCommunities,
+	handleJoinCommunity,
 } from "../controllers/communityController";
 
 const router = Router();
 
 router.get("/fetch", handleFetchCommunities);
 router.post("/create", handleCommunityCreation);
+router.put("/join", handleJoinCommunity);
 
 export default router;
