1e543adc8c89a711e4af5966c74071278448eada
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-02-23 18:13:13 +0530

    make cors whitelist configurable via env vars

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,9 +1,9 @@
 import { CorsOptions } from "cors";
 
-const whiteList = ["http://localhost:3000"];
+const whiteList = [process.env.FRONTEND_URL_FOR_CORS as string];
 const corsOptions: CorsOptions = {
 	origin: function (origin, callback) {
-		if ((origin && whiteList.indexOf(origin) !== -1) || !origin) {
+		if (origin && whiteList.indexOf(origin) !== -1) {
 			callback(null, true);
 		} else {
 			callback(new Error("Not allowed by CORS"));
