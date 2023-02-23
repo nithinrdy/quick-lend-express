f7fd82940874ff8e60e9bf54bb2ac8438b617643
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-02-23 18:54:11 +0530

    array isn't useful anymore anyway

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,9 +1,9 @@
 import { CorsOptions } from "cors";
 
-const whiteList = [process.env.FRONTEND_URL_FOR_CORS as string];
 const corsOptions: CorsOptions = {
 	origin: function (origin, callback) {
-		if (origin && whiteList.indexOf(origin) !== -1) {
+		const allowedOrigin = process.env.FRONTEND_URL_FOR_CORS as string;
+		if (origin && allowedOrigin === origin) {
 			callback(null, true);
 		} else {
 			callback(new Error("Not allowed by CORS"));
